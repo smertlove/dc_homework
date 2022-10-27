5c75e155f0b5b405673e4c6bb01f10c76c987180
Kirill Soloshenko <smertlove@yandex.ru>
2022-10-27 22:31:37 +0300

    убрал дебаг из ех2

--- ex2/ex2.py
+++ ex2/ex2.py
@@ -1,16 +1,6 @@
 from functools import reduce
 
 def lists_sum(*lists, unique=False) :
-
-    # if unique:
-    #     return sum(
-    #         reduce(
-    #             lambda st1, st2: st1 | st2,
-    #              map(set, [*lists, [0]])
-    #              )
-    #         )
-
-    # return sum([sum(list) for list in [*lists, [0]]])
     return (sum(reduce(lambda st1, st2: st1 | st2,map(set, [*lists, [0]])))if unique else sum([sum(list) for list in [*lists, [0]]])) if len(lists) > 0 else 0 
 
 def main():
