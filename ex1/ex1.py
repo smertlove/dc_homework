17e2d956ac011eaae9a6eb92bf032b07aa7093b3
Kirill Soloshenko <smertlove@yandex.ru>
2022-10-27 22:17:53 +0300

    теперь ех1 работает в контесте

--- ex1/ex1.py
+++ ex1/ex1.py
@@ -1,13 +1,24 @@
 import datetime
 
 def gift_count(budget, month, birthdays):
-    print(f"""Именинники в месяце {month}: {', '.join((birthday_boys := [f"{boy} ({datetime.datetime.strftime(birthdays[boy], '%d.%m.%Y')})" for boy in birthdays if birthdays[boy].month == month]))}. При бюджете {budget} они получат по {budget // len(birthday_boys)} рублей.""")
+    print(f"""Именинники в месяце {month}: {', '.join((birthday_boys := [f"{boy} ({datetime.datetime.strftime(bdate, '%d.%m.%Y')})" for boy, bdate in sorted(tuple(birthdays.items()), key=lambda c: c[1].day) if birthdays[boy].month == month]))}. При бюджете {budget} они получат по {budget // len(birthday_boys)} рублей.""" )
+
+
+# def gift_count(budget, month, birthdays):    
     
+#     birthday_boys = [f"{boy} ({datetime.datetime.strftime(bdate, '%d.%m.%Y')})" for boy, bdate in sorted(tuple(birthdays.items()), key=lambda c: c[1].day) if birthdays[boy].month == month]
+    
+#     if len(birthday_boys) == 0:
+#         print("В этом месяце нет именниннков.")
+#         return
+    
+#     print(f"""Именинники в месяце {month}: {', '.join(birthday_boys)}. При бюджете {budget} они получат по {budget // len(birthday_boys)} рублей.""")
 
 def main():
     birthdays = {"Иванов Иван Иванович": datetime.date(1989, 5, 1), "Петров Петр Петрович": datetime.date(1998, 5, 6)}
 
     gift_count(20000, 5, birthdays)
+    gift_count(20000, 5, {})
 
     gift_count(budget=20000, month=5, birthdays=birthdays)
 
