58ffe41780b7b564f0ff7acb69a4b868d3cc3be2
Kirill Soloshenko <smertlove@yandex.ru>
2022-10-27 17:32:52 +0300

    ex1 made

--- ex1/ex1.py
+++ ex1/ex1.py
@@ -1,5 +1,17 @@
+import datetime
+
+def gift_count(budget, month, birthdays):
+    birthday_boys = [f"{boy} ({datetime.datetime.strftime(birthdays[boy], '%d.%m.%Y')})" for boy in birthdays if birthdays[boy].month == month]
+    
+    print(f"Именинники в месяце {month}: {', '.join(birthday_boys)}. При бюджете {budget} они получат по {budget // len(birthday_boys)} рублей.")
+    
+
 def main():
-    pass
+    birthdays = {"Иванов Иван Иванович": datetime.date(1989, 5, 1), "Петров Петр Петрович": datetime.date(1998, 5, 6)}
+
+    gift_count(20000, 5, birthdays)
+
+    gift_count(budget=20000, month=5, birthdays=birthdays)
 
 if __name__ == "__main__":
     main()
