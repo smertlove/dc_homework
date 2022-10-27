0d2a763384d70ad34ffe9699cd1ddd9b3aae54cd
Kirill Soloshenko <smertlove@yandex.ru>
2022-10-27 23:16:37 +0300

    ех3 теперь проходит на контесте

--- ex3/ex3.py
+++ ex3/ex3.py
@@ -4,7 +4,7 @@ def get_balance(name, transactions):
     return sum((c["amount"] for c in transactions if c["name"] == name))
 
 def count_debts(names, amount, transactions):
-    return {name: ((debt := amount - get_balance(name, transactions)) > 0) * debt for  name in names}
+    return {name: max(amount - get_balance(name, transactions), 0) for  name in names}
 
 
 def main():
@@ -12,6 +12,14 @@ def main():
 
     print(get_balance("Василий", transactions))
 
+    print(count_debts(["Василий", "Петя", "Вова"], 150, transactions))
+
+    transactions = [ 
+        {"name": "Василий", "amount": 500},
+        {"name": "Петя", "amount": 100},
+        {"name": "Василий", "amount": -300}, 
+        {"name": "Петя", "amount": -500}
+    ]
     print(count_debts(["Василий", "Петя", "Вова"], 150, transactions))
 
 if __name__ == "__main__":
