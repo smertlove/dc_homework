e2b4a6110186f55d964195731c1c86de24142055
Kirill Soloshenko <smertlove@yandex.ru>
2022-10-27 18:28:23 +0300

    ex2 done

--- ex2/ex2.py
+++ ex2/ex2.py
@@ -1,5 +1,17 @@
+from functools import reduce
+
+def lists_sum(*lists, unique=False) :
+    if unique:
+        return sum(reduce(lambda st1, st2: st1 | st2, tuple(map(set, lists))))
+    return sum([sum(list) for list in lists])
+
 def main():
-    pass
+    
+    print(lists_sum([1, 1], [1], [1, 2, 3]))
+
+    print(lists_sum([1, 1, 1], [1, 1], unique=True))
+
+    print(lists_sum([1, 1, 1], unique=False))
 
 if __name__ == "__main__":
     main()
