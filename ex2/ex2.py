209696cad7e1b73526f191bd998ad7c0b4701bb7
Kirill Soloshenko <smertlove@yandex.ru>
2022-11-02 14:35:38 +0300

    done ex2 and ex3

--- ex2/ex2.py
+++ ex2/ex2.py
@@ -1,5 +1,28 @@
+import os
+
+def popular_name_from_generator(data):
+    freq = {}
+    temp_max = 0  ## чтобы не искать максимум лишний раз
+    for elem in data:
+        freq[elem] = freq.pop(elem, 0) + 1
+        if temp_max < freq[elem]:
+            temp_max = freq[elem]
+    return ', '.join(sorted([k for k in freq if freq[k] == temp_max]))
+
+def get_popular_name_from_file(filename):
+    with open(filename, "r", encoding="utf-8") as file:
+        data = map(lambda c: c.split()[0], file.read().strip().split('\n'))
+    return popular_name_from_generator(data)
+
+
 def main():
-    pass
+    test_dir = os.path.join( os.path.dirname(__file__), "tests" )
+    test_paths = ["1.txt", "2.txt", "3.txt"]
+    for path in  test_paths:
+        print(f"{path}:\n")
+        print(get_popular_name_from_file(os.path.join(test_dir, path)))
+        print("- - - - -\n")
+
 
 if __name__ == "__main__":
     main()
--- ex3/ex3.py
+++ ex3/ex3.py
@@ -1,5 +1,30 @@
+import json
+
+
+
+def mean_age(json_string):
+    ages = [obj["age"] for obj in json.loads(json_string)]
+    return json.dumps({"mean_age": sum(ages)/len(ages)})
+
+
 def main():
-    pass
+    testcase = '''[
+    {
+        "name": "Петр",
+        "surname": "Петров",
+        "patronymic": "Васильевич",
+        "age": 23,
+        "occupation": "ойтишнек"
+    },
+    {
+        "name": "Василий",
+        "surname": "Васильев",
+        "patronymic": "Петрович",
+        "age": 24,
+        "occupation": "дворник"
+    }
+]'''
+    print(mean_age(testcase))
 
 if __name__ == "__main__":
     main()
