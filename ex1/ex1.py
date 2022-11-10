185876be1a8219bf3e0ec6ca49e486ccf8fd1cd7
Kirill Soloshenko <smertlove@yandex.ru>
2022-11-10 20:32:00 +0300

    tests for ex1 moved to a different file

--- ex1/ex1.py
+++ ex1/ex1.py
@@ -1,7 +1,3 @@
-import unittest
-
-
-
 class Calculator:
 
     last = None
@@ -46,49 +42,3 @@ class Calculator:
     def clear(cls):
         cls.last = None
 
-
-
-class TestCalculator(unittest.TestCase):
-    
-    def setUp(self):
-        self.calc = Calculator()
-
-    def test_sum(self):
-        self.assertEqual(self.calc.sum(4, 8), 12)
-
-    def test_sub(self):
-        self.assertEqual(self.calc.sub(70, 30), 40)
-
-    def test_mul(self):
-        self.assertEqual(self.calc.mul(2, 2), 4)
-
-    def test_div(self):
-        self.assertEqual(self.calc.div(50, 2), 25)
-
-    def test_history(self):
-        self.calc.sum(4, 8)
-        self.calc.sub(70, 30)
-        self.calc.mul(2, 2)
-        self.calc.div(50, 2)
-        self.assertEqual(self.calc.history(2), "mul(2, 2) == 4")
-
-    def test_last(self):
-        self.calc.sum(4, 8)
-        self.calc.sub(70, 30)
-        self.calc.mul(2, 2)
-        self.calc.div(50, 2)
-        f = Calculator()
-        self.assertEqual(f.last, "div(50, 2) == 25.0")
-
-    def test_clear(self):
-        self.calc.clear()
-        self.assertEqual(Calculator.last, None)
-
-
-def main(): 
-    unittest.main()
-    
-   
-
-if __name__ == "__main__":
-    main()
---
+++ ex1/test.py
@@ -0,0 +1,47 @@
+import unittest
+from ex1 import Calculator
+
+class TestCalculator(unittest.TestCase):
+    
+    def setUp(self):
+        self.calc = Calculator()
+
+    def test_sum(self):
+        self.assertEqual(self.calc.sum(4, 8), 12)
+
+    def test_sub(self):
+        self.assertEqual(self.calc.sub(70, 30), 40)
+
+    def test_mul(self):
+        self.assertEqual(self.calc.mul(2, 2), 4)
+
+    def test_div(self):
+        self.assertEqual(self.calc.div(50, 2), 25)
+
+    def test_history(self):
+        self.calc.sum(4, 8)
+        self.calc.sub(70, 30)
+        self.calc.mul(2, 2)
+        self.calc.div(50, 2)
+        self.assertEqual(self.calc.history(2), "mul(2, 2) == 4")
+
+    def test_last(self):
+        self.calc.sum(4, 8)
+        self.calc.sub(70, 30)
+        self.calc.mul(2, 2)
+        self.calc.div(50, 2)
+        f = Calculator()
+        self.assertEqual(f.last, "div(50, 2) == 25.0")
+
+    def test_clear(self):
+        self.calc.clear()
+        self.assertEqual(Calculator.last, None)
+
+
+def main(): 
+    unittest.main()
+    
+   
+
+if __name__ == "__main__":
+    main()
