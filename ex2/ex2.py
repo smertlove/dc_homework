b6121ae9910e69251aadc5172ae244e013b730d3
Kirill Soloshenko <smertlove@yandex.ru>
2022-11-15 22:40:57 +0300

    redone ex2

--- ex2/ex2.py
+++ ex2/ex2.py
@@ -5,34 +5,34 @@ EURO_COEF = 70
 
 class BaseWallet:
 
-    _coefficient = 1
+    exchange_rate = 1
     _log_name = "Base Wallet"
 
     def __init__(self, name, amount):
-        self._name = name
-        self._amount = amount
+        self.name = name
+        self.amount = amount
 
     def get_amount(self):
-        return self._amount
+        return self.amount
 
     def get_name(self):
-        return self._name
+        return self.name
 
-    def get_coefficient(self):
-        return self._coefficient
+    def getexchange_rate(self):
+        return self.exchange_rate
 
     def to_base(self):
-        return self._amount * self._coefficient
+        return self.amount * self.exchange_rate
 
     # Повторяющийся код математических операций. #
     def __apply_operation(self, other, operation):
         if isinstance(other, BaseWallet):
-            return self.__class__(self._name,
+            return self.__class__(self.name,
                                   operation(
                                       self.to_base(),
                                       other.to_base()
-                                      ) // self._coefficient)
-        return self.__class__(self._name, operation(self._amount, other))
+                                      ) / self.exchange_rate)
+        return self.__class__(self.name, operation(self.amount, other))
 
     def __add__(self, other):
         return self.__apply_operation(other, lambda a, b: a + b)
@@ -68,42 +68,29 @@ class BaseWallet:
         return self.__apply_operation(other, lambda a, b: b / a)
 
     def __eq__(self, other):
-        return type(self) is type(other) and self._amount == other.get_amount()
+        return type(self) is type(other) and self.amount == other.get_amount()
 
     def __repr__(self):
-        return f"{self._log_name} {self._name} {self._amount}"
+        return f"{self._log_name} {self.name} {self.amount}"
 
     def spend_all(self):
         if self.get_amount() > 0:
-            self._amount = 0
+            self.amount = 0
 
 
 class RubbleWallet(BaseWallet):
 
-    _coefficient = RUBLE_COEF
+    exchange_rate = RUBLE_COEF
     _log_name = "Rubble Wallet"
 
 
 class DollarWallet(BaseWallet):
 
-    _coefficient = DOLLAR_COEF
+    exchange_rate = DOLLAR_COEF
     _log_name = "Dollar Wallet"
 
 
 class EuroWallet(BaseWallet):
 
-    _coefficient = EURO_COEF
+    exchange_rate = EURO_COEF
     _log_name = "Euro Wallet"
-
-
-def main():
-    a = RubbleWallet("X", 20)
-    b = DollarWallet("D", 10)
-    c = a + b
-    d = RubbleWallet("X", 620)
-    for i in (a, b, c, d):
-        print(i)
-
-
-if __name__ == "__main__":
-    main()
