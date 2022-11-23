34404ac6df38dcca439cac077dcdfb43fcf9191a
Kirill Soloshenko <smertlove@yandex.ru>
2022-11-23 21:13:31 +0300

    ex1 and ex2 done

--- ex3/ex3.py
+++ ex3/ex3.py
@@ -1,5 +1,9 @@
+import requests
+from pprint import pprint
+
 def main():
-    pass
+    responce = requests.get("https://jsonplaceholder.typicode.com/posts")
+    pprint(responce)
 
 if __name__ == "__main__":
     main()
