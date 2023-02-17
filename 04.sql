1bc6cc2d744c0ed9f7181ab694bc42e3fac4283d
Kirill Soloshenko <smertlove@yandex.ru>
2023-02-17 21:36:15 +0300

    5 done

--- 04.sql
+++ 04.sql
@@ -3,6 +3,7 @@
 Мы хотим создать карту, в какие магазины ходит каждый конкретный покупатель.
 Выберите все пары customer_id, store_id для этого.
 */
+
 SELECT
     DISTINCT customer_id, store_id
 FROM
